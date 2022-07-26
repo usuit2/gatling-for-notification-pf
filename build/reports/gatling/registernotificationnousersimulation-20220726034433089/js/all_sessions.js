982e8c918558803d9c78e2e9db1a39e611560b69
EC2 Default User <ec2-user@ip-10-102-147-183.ap-northeast-1.compute.internal>
2022-07-26 03:47:05 +0000

    100 per sec

---
+++ build/reports/gatling/registernotificationnousersimulation-20220726034433089/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1658807074000,10]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
